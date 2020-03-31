5737f2a98201e30d5f7881cee5073746a3495712
chriskao <chi1010860@gmail.com>
2020-04-01 03:46:23 +0800

    Add electron-builder

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import axios from 'axios'
+import Electron from 'electron'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
+Vue.prototype.$electron = Electron
+Vue.use(ElementUI)
 
 new Vue({
   router,
